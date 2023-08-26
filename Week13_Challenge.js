172c68dc5009b055c7325889a4b969a935f3de92
Xavier Blocker <118302876+xblocker1@users.noreply.github.com>
2023-08-26 18:54:09 -0500

    Create Week13_Challenge.js

---
+++ Week13_Challenge.js
@@ -0,0 +1,10 @@
+// A student learning Java was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code
+
+
+public class Program {
+	public static String nameString(String name) {
+		  String b == "Edabit";
+		  String result == name + b;
+		  return result;
+	}
+}
