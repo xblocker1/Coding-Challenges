d03a5be4a549563edc9add353fcc353753c9efe5
Xavier Blocker <118302876+xblocker1@users.noreply.github.com>
2023-08-29 09:15:27 -0500

    Create Week14_Challenge.js

---
+++ Week14_Challenge.js
@@ -0,0 +1,12 @@
+// Create a function that returns true when x is equal to y; otherwise return false.
+
+public class Program {
+	public static boolean isSameNum(int x, int y) {
+		if (x==y){
+			return true;
+			else{
+				return false;
+			}
+		}
+	}
+}
