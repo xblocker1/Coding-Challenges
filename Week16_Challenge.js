06af036c42877c74623fee30051b5d8da7061e70
Xavier Blocker <118302876+xblocker1@users.noreply.github.com>
2023-09-04 12:30:22 -0500

    Create Week16_Challenge.js

---
+++ Week16_Challenge.js
@@ -0,0 +1,7 @@
+// Create a method that takes two integers as arguments and returns their sum.
+
+public class SumOfTwoNumbers {
+    public static int sum(int a, int b) {
+      return a + b;
+    }
+}
